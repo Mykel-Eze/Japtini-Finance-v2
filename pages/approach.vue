ca45e470450b31263b15c1f83f7510d6c778f665
Mykel-Eze <michaeleze1997@gmail.com>
2023-01-18 13:32:35 +0100

    approach page done

---
+++ pages/approach.vue
@@ -0,0 +1,384 @@
+<template>
+  <div id="approach-page" class="other-pages">
+    <!---=== Home Section ===--->
+    <section id="home">
+      <div class="row full-height">
+        <div class="container full-height">
+          <div class="home-txt-wrapper">
+            <h1 class="home-txt-title">
+              Our Approach
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!---=== end of Home Section ===--->
+
+    <!---=== Last-Mile Section ===--->
+    <section id="last-mile">
+      <div class="row">
+        <div class="container">
+          <div class="mid-sect-wrapper col s12">
+            <h4 class="wpb-sect-heading">
+              The last mile challenge
+            </h4>
+
+            <div class="sect-contents">
+              <div class="int-img-side rel text-center-xs" data-aos="fade-up">
+                <img src="~/assets/images/mile-1.jpg" alt="mile-image" class="int-img active-int-img">
+                <img src="~/assets/images/mile-2.jpg" alt="mile-image" class="int-img">
+                <img src="~/assets/images/mile-3.jpg" alt="mile-image" class="int-img">
+                <img src="~/assets/images/mile-4.jpg" alt="mile-image" class="int-img">
+                <img src="~/assets/images/green-rect.svg" alt="green-rect" class="green-rect">
+              </div>
+
+              <div class="int-txt-side">
+                <ul class="int-txt-indicator">
+                  <li class="txt-indicator-block active-indicator-block">
+                    <span class="indicator-text">
+                      Far too many of us in Africa are power generators. As individuals – householders,
+                      farmers, business people – we purchase diesel generators as backup to unreliable or
+                      unavailable grid connections.
+                    </span>
+                  </li>
+                  <li class="txt-indicator-block">
+                    <span class="indicator-text">
+                      This is not the way it should work. By coming together to purchase energy, we can achieve
+                      better prices, better terms of credit and more sustainable and reliable energy.
+                    </span>
+                  </li>
+                  <li class="txt-indicator-block">
+                    <span class="indicator-text">
+                      This is the principle behind Japtini Asset Finance. As a bulk buyer of solar energy, farm
+                      machinery and transport equipment, we obtain the best assets at the cheapest cost and
+                      lowest rates of credit.
+                    </span>
+                  </li>
+                  <li class="txt-indicator-block">
+                    <span class="indicator-text">
+                      Join us on this transformational journey.
+                    </span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!---=== end of Last-Mile Section ===--->
+
+    <!---=== Earning-Priority Section ===--->
+    <section id="earning-priority">
+      <div class="row">
+        <div class="container">
+          <div class="earning-priority-wrapper">
+            <div class="epw-txt-side">
+              <h4 class="section-title pry-color">
+                Your earnings are our priority
+              </h4>
+              <div class="epw-txt-desc">
+                <p>
+                  Our entire objective at Japtini Asset Finance is to ensure that you receive sufficient
+                  return on investment to repay your credit – and invest again in further assets.
+                </p>
+                <p>
+                  We are aligned in finding you the best price and credit terms for your assets, and then working
+                  with you to extract their full potential – both in earnings and impact.
+                </p>
+                <p>
+                  By combining our best knowledge of the market with the preferences and priorities of asset
+                  investors, we ensure the right equipment is purchased for optimal utility.
+                </p>
+              </div>
+            </div>
+            <div class="epw-img-side">
+              <img src="~/assets/images/rich-woman.jpg" alt="woman with money" class="epw-img">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!---=== end of Earning-Priority Section ===--->
+
+    <!---=== Q-and-A Section ===--->
+    <section id="q-and-a">
+      <div class="row">
+        <div class="container">
+          <h4 class="section-title center">
+            Questions and Answers
+          </h4>
+
+          <div class="qa-wrapper">
+            <div class="qa-block">
+              <div class="qa-img-side">
+                <img src="~/assets/images/fuel-pump.jpg" alt="fuel" class="qa-img">
+              </div>
+              <div class="qa-txt-side">
+                <div class="qa-txt-title pry-color">
+                  How do rising energy costs impact my return on investment (ROI)?
+                </div>
+                <div class="qa-txt-desc">
+                  The rising cost of diesel and electricity have triggered unprecedented demand for solar
+                  powered systems for farms, businesses and homes.
+                </div>
+                <div class="qa-txt-side">
+                  While higher fuels costs increase the operating costs of trucks and farming machinery,
+                  this does not reduce demand for the equipment. After all, crops still need to be planted
+                  and harvests need to be collected and transported.
+                </div>
+              </div>
+            </div>
+            <div class="qa-block">
+              <div class="qa-txt-side">
+                <div class="qa-txt-title pry-color">
+                  What happens if my truck or tractor breaks down?
+                </div>
+                <div class="qa-txt-desc">
+                  All assets are 100% covered by warranties and insurance, enabling prompt repair or replacement at
+                  no cost to the owner.
+                </div>
+              </div>
+              <div class="qa-img-side">
+                <img src="~/assets/images/truck-breakdown.jpg" alt="truck-breakdown" class="qa-img">
+              </div>
+            </div>
+            <div class="qa-block">
+              <div class="qa-img-side">
+                <img src="~/assets/images/dashboard-screenshot.png" alt="dashboard-screenshot" class="qa-img">
+              </div>
+              <div class="qa-txt-side">
+                <div class="qa-txt-title pry-color">
+                  Where should I locate my assets for maximum uptake?
+                </div>
+                <div class="qa-txt-desc">
+                  We share in-depth statistics on farmers and other equipment leaseholders across our network to
+                  enable informed decision making by investors. Japtini Asset Finance is with you every step of
+                  the way via our personal consultants and comprehensive app.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!---=== end of Q-and-A Section ===--->
+
+    <!---=== Integrated-Network Section ===--->
+    <section id="integrated-network">
+      <div class="row">
+        <div class="container">
+          <div class="integrated-network-wrapper">
+            <div class="inw-title-side">
+              Integrated Network
+            </div>
+            <div class="inw-desc-side">
+              <p>
+                Japtini Asset Finance is part of a network of nine standalone solutions providers, each with the
+                common purpose of supporting communities and enabling farmers and SMEs to increase output.
+              </p>
+              <p>
+                Each of the nine businesses interconnect and synergise to enable Japtini Asset Finance to
+                optimise its services.
+              </p>
+              <p>
+                Our network of solutions providers operate in the fields of agriculture, crop services, energy,
+                logistics, haulage, financial technology and insurance.
+              </p>
+              <p class="bold-txt">
+                Together, we build dynamic partnerships for mutual problem solving.
+              </p>
+            </div>
+          </div>
+          <div class="sect-contents marquee-content">
+            <div class="marquee-item">
+              <img src="~/assets/images/integrated-network.png" alt="integrated-network" class="full-width">
+            </div>
+            <div class="marquee-item">
+              <img src="~/assets/images/integrated-network.png" alt="integrated-network" class="full-width">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!---=== end of Integrated-Network Section ===--->
+
+    <!---=== Arila-Companies Section ===--->
+    <section id="arila-companies">
+      <div class="row">
+        <div class="container">
+          <div id="arila-block-1" class="company-block-wrapper visible-xs-sm">
+            <div class="center">
+              <img src="~/assets/images/japtini-finance.png" alt="japtini-finance" class="top-company-logo">
+            </div>
+            <div class="company-block">
+              <img src="~/assets/images/arila-white.png" alt="Arila" class="company-image">
+              <div class="company-desc">
+                <span class="arila-txt">Arila</span> connects, collaborates and coordinates between all of the
+                operating companies’ services, providing planning and support. Additionally, Arila provides
+                expertise in finance, technology, regulations, human resources and marketing.
+              </div>
+            </div>
+          </div>
+          <div class="arila-companies-wrapper">
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/alluvial.png" alt="alluvial" class="company-image">
+                <div class="company-name">
+                  Farming services
+                </div>
+                <div class="company-desc">
+                  <b>Alluvial Agriculture</b> provides training, technology, land preparation, irrigation, input
+                  supplies and market access within contiguous farmlands in community blocks
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/fadhila.png" alt="fadhila" class="company-image">
+                <div class="company-name">
+                  Crop protection & nutrients
+                </div>
+                <div class="company-desc">
+                  <b>Fadhila</b> designs crop protection and assesses crop nutrient requirements, and then commissions
+                  producers to provide quality inputs to the specifications of the farmers
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/japtini-food.png" alt="japtini-food" class="company-image">
+                <div class="company-name">
+                  Food & Retail
+                </div>
+                <div class="company-desc">
+                  <b>Japtini Food & Retail</b> aggregates the primary produce and takes it to the marketplace
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper jl">
+              <div class="company-block">
+                <img src="~/assets/images/japtini-energy.png" alt="japtini-energy" class="company-image">
+                <div class="company-name">
+                  Japtini Energy
+                </div>
+                <div class="company-desc">
+                  <b>Japtini Energy</b> provides solar panels and batteries for affordable and reliable power
+                </div>
+              </div>
+            </div>
+            <div id="arila-block" class="company-block-wrapper hidden-xs-sm">
+              <div class="center">
+                <img src="~/assets/images/japtini-finance.png" alt="japtini-finance" class="top-company-logo">
+              </div>
+              <div class="company-block">
+                <img src="~/assets/images/arila-white.png" alt="Arila" class="company-image">
+                <div class="company-desc">
+                  <span class="arila-txt">Arila</span> connects, collaborates and coordinates between all of the
+                  operating companies’ services, providing planning and support. Additionally, Arila provides
+                  expertise in finance, technology, regulations, human resources and marketing.
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper jl">
+              <div class="company-block">
+                <img src="~/assets/images/japtini-logistics.png" alt="japtini-logistics" class="company-image">
+                <div class="company-name">
+                  Cargo & Transport
+                </div>
+                <div class="company-desc">
+                  <b>Japtini Logistics</b> uses advanced technology to open access to third party logistics service providers
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/angala-insuretech.png" alt="alluvial" class="company-image">
+                <div class="company-name">
+                  Insurance
+                </div>
+                <div class="company-desc">
+                  <b>Angala Insurtech</b> uses technology to source and retain the best insurance policies
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/japtini-finance.png" alt="japtini-finance" class="company-image">
+                <div class="company-name">
+                  Asset Finance
+                </div>
+                <div class="company-desc">
+                  <b>Japtini Asset Finance</b> helps companies purchase equipment such as trucks or combine harvesters by
+                  working with financial institutions and service providers
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/angala-fintech.png" alt="angala-fintech" class="company-image">
+                <div class="company-name">
+                  Banking & credit
+                </div>
+                <div class="company-desc">
+                  <b>Angala Fintech</b> brings banking services to rural and other unbanked communities through financial
+                  literacy, small and micro lending, and payment gateways for online payments and cash withdrawals
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!---=== end of Arila-Companies Section ===--->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ApproachPage',
+  mounted () {
+    // Key-Points Section --- Title Carousel
+    let titleIndex = 0
+    titleCarousel()
+
+    function titleCarousel () {
+      let i
+      const x = document.getElementsByClassName('int-img')
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'none'
+      }
+      titleIndex++
+      if (titleIndex > x.length) { titleIndex = 1 }
+      x[titleIndex - 1].style.display = 'block'
+      setTimeout(titleCarousel, 4000) // Change image every 2 seconds
+    }
+    // Key-Points Section --- Title Carousel
+    let titleIndex2 = 0
+    titleCarousel2()
+
+    function titleCarousel2 () {
+      let i
+      const x = document.getElementsByClassName('txt-indicator-block')
+      for (i = 0; i < x.length; i++) {
+        x[i].classList.remove('active-indicator-block')
+      }
+      titleIndex2++
+      if (titleIndex2 > x.length) { titleIndex2 = 1 }
+      x[titleIndex2 - 1].classList.add('active-indicator-block')
+      setTimeout(titleCarousel2, 4000) // Change image every 2 seconds
+    }
+  }
+}
+</script>
+
+<style src="../assets/css/home.css"></style>
+<style src="../assets/css/approach.css"></style>
+
+<style scoped>
+#home {
+  background-image: url("../assets/images/hero-image-approach.jpg");
+  height: 70vh;
+  min-height: 400px;
+}
+</style>
